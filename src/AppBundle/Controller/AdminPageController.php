75945b50f639cd1499b9ae828a7531aaee57d58e
J0hnLem0n <nikolaevee@gmail.com>
2015-07-26 16:53:19 +0600

    add insert categoryPeople

--- src/AppBundle/Controller/AdminPageController.php
+++ src/AppBundle/Controller/AdminPageController.php
@@ -7,13 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\RequestPeople;
 use AppBundle\Entity\RequestCategory;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminPageController extends Controller
 {
 //    /**
 //     * @Route("/admin")
 //     */
-    public function showAdminPageAction($value)
+    public function showAdminPageAction(Request $request, $value)
     {
        // return new Response('Admin page!');
         if ($value == 'main') {
@@ -22,9 +23,30 @@ class AdminPageController extends Controller
             return $this->render('AppBundle:adminMenu:RequestPeople.html.twig', array('RequestPeople' => $requestPeople));
         }
         if ($value == 'requestCategory') {
+            $RequestCategory = new RequestCategory();
+            $form = $this->createFormBuilder($RequestCategory)
+                ->add('name', 'text')
+                ->add('save', 'submit', array('label' => 'Сохранить', 'attr' => array('class'=>'btn btn-primary')))
+                ->getForm();
+
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($RequestCategory);
+                $em->flush();
+//                $url = $this->get('router')->generate('blog_show', array('slug' => 'my-blog-post'));
+//                return $this->render('AppBundle:adminMenu:RequestCategory.html.twig', array('value' => 'main'));
+//                return $this->render('AppBundle:adminMenu:RequestCategory.html.twig', array('RequestCategory' => $requestCategory));
+               return $this->redirectToRoute('admin_page', ['value' => 'requestCategory']);
+            }
             $em = $this->getDoctrine()->getManager()->getRepository('AppBundle:RequestCategory');
             $requestCategory = $em->findBy(array(), array('id' => 'ASC'));
-            return $this->render('AppBundle:adminMenu:RequestCategory.html.twig', array('RequestCategory' => $requestCategory));
+            return $this->render('AppBundle:adminMenu:RequestCategory.html.twig', array('RequestCategory' => $requestCategory,'form' => $form->createView()));
+//            return $this->render('AppBundle:Default:index.html.twig', array(
+//                'form' => $form->createView(),
+//            ));
+
+
         }
     }
 }
